c9e1169f5191f52c108011439e474bdf5e6e5bc6
Simon <simonfong6@gmail.com>
2020-06-21 00:52:13 -0700

    toggle button

--- app/src/components/Toggle/Toggle.jsx
+++ app/src/components/Toggle/Toggle.jsx
@@ -4,12 +4,29 @@ class Toggle extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      on: true
+    }
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    const { on } = this.state;
+  
+    this.setState({
+      on: !on
+    });
   }
 
   render() {
+    const { on } = this.state;
+  
     return (
-      <button>
-        On
+      <button
+        onClick={this.toggle} >
+        {on ? 'On' : 'Off'}
       </button>
     );
   }
