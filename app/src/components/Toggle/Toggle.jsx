14aefd6b9db4b0db05b1d4a316e82f6e5697a1ce
Simon <simonfong6@gmail.com>
2020-06-21 00:45:11 -0700

    Toggle

---
+++ app/src/components/Toggle/Toggle.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Toggle {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button>
+        On
+      </button>
+    );
+  }
+}
+
+export default Toggle;
